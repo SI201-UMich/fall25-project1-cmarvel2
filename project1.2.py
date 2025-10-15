7c2ebb885902fd35369adf47ec7e693590c06bf2
cmarvel2 <cmarvel@umich.edu>
2025-10-15 19:46:46 -0400

    Genarate _store_analysis was implemented Oct. 14 to write results to a txt file, TestCases were implemented Oct 15

--- project1.2.py
+++ project1.2.py
@@ -2,7 +2,7 @@
 # Student ID: 3620 4751
 # Email: cmarvel@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
-# Asked ChatGPT hints for debugging and methods/workaround to use when code wasn't performing as it should've
+# Asked ChatGPT hints for debugging and methods/workaround i.e round(float(),2) to use when code wasn't performing as it should've, formatting txt file output, and Unittest cases
 
 import csv
 import unittest
@@ -17,6 +17,7 @@ class Data_Reader:
 
         self.base_path = os.path.abspath(os.path.dirname(__file__))
         self.full_path = os.path.join(self.base_path, file)
+
 
     def load_datasheet(self):
 
@@ -36,7 +37,6 @@ class Data_Reader:
                 profitdict[row[4]] = [float(row[9]), float(row[12]),] 
             else:
                 profitdict[row[4]] += [float(row[9]), float(row[12])]
-
 
         #Implemented due to round(float()) returning values with 2.8999999999999 instead ot 2.90 because of 
         for key, value in profitdict.items():
@@ -118,16 +118,68 @@ class Data_Reader:
                     
                 
     def generate_store_analysis(self):
-        profitdata = self.calculate_state_profits
-        itemsdata = self.state_most_products
+        profitdata = self.calculate_state_profits()
+        itemsdata = self.state_most_products()
         with open("analysis.txt", "w") as ofile:
-            pass
+            ofile.write("State Profit Analysis\n\n")
+            pprint.pprint(profitdata, stream=ofile,)
+
+            ofile.write("\n\nState Most Sold Products\n\n")
+            pprint.pprint(itemsdata, stream=ofile,)
                 
-            
+class project1_2(unittest.TestCase):
+    def setUp(self):
+        self.Teststoredata = Data_Reader('SampleSuperstore.csv')
 
+    #State profits test    
+    
+    def test_calculate_state_profits_type(self):
+        result = self.Teststoredata.calculate_state_profits()
+        self.assertIsInstance(result, dict)
 
+    def test_calculate_state_profits_has_state(self):
+        result = self.Teststoredata.calculate_state_profits()
+        # Normal case: should contain some known state
+        self.assertIn("California", result)
 
+    def test_calculate_state_profits_structure(self):
+        result = self.Teststoredata.calculate_state_profits()
+        # Each value should be a list of 4 formatted strings
+        first_key = next(iter(result))
+        self.assertEqual(len(result[first_key]), 4)
 
+    def test_calculate_state_profits_losscheck(self):
+        result = self.Teststoredata.calculate_state_profits()
+        # Edge case: check that the loss boolean is included
+        first_key = next(iter(result))
+        self.assertTrue(any("Was there a loss" in s for s in result[first_key]))
+
+    #Most frequent product check
+    
+    def test_state_most_products_type(self):
+        result = self.Teststoredata.state_most_products()
+        self.assertIsInstance(result, dict)
+
+    def test_state_most_products_nested_structure(self):
+        result = self.Teststoredata.state_most_products()
+        first_state = next(iter(result))
+        self.assertIsInstance(result[first_state][0], dict)
+
+    def test_state_most_products_keys_exist(self):
+        result = self.Teststoredata.state_most_products()
+        first_state = next(iter(result))
+        first_city = next(iter(result[first_state][0]))
+        self.assertIsInstance(first_city, str)
+
+    def test_state_most_products_counts_positive(self):
+        result = self.Teststoredata.state_most_products()
+        first_state = next(iter(result))
+        first_city = next(iter(result[first_state][0]))
+        counts = result[first_state][0][first_city]
+        # Edge case: counts should be a list with 2 dicts (segment + category)
+        self.assertEqual(len(counts), 2)
+
+    
 
 def main():
     Storedata = Data_Reader('SampleSuperstore.csv')
@@ -136,8 +188,7 @@ def main():
     Storedata.prepare_state_list()
     Storedata.state_most_products()
     Storedata.generate_store_analysis()
-    
+    unittest.main(verbosity=2)
 
 if __name__ == '__main__':
     main()
-    unittest.main(verbosity=2)
